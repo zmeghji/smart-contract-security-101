19c68b404e6b4975b52c86ed704005b41781a67a
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2022-01-16 00:40:59 -0500

    completed module 7 (tx.origin)

--- test-fill-the-blanks/tx-origin-test.js
+++ test-fill-the-blanks/tx-origin-test.js
@@ -21,8 +21,31 @@ describe("Tx.origin", function () {
       expect(await ethers.provider.getBalance(this.smallWallet.address)).to.eq(10000);
     });
     
-    it("...", async function(){
+    it("Should allow owner to execute withdrawal", async function(){
+      const initialUserBalance = await ethers.provider.getBalance(user.address);
+
+      await this.smallWallet.withdrawAll(user.address);
+      expect(await ethers.provider.getBalance(this.smallWallet.address)).to.eq(0)
+      expect(await ethers.provider.getBalance(user.address)).to.eq(initialUserBalance.add(10000))
 
     });
+
+    it ("Should revert if withdraw all is called from someone other than the owner",
+      async function(){
+        await expect(this.smallWallet.connect(attacker).withdrawAll(attacker.address))
+          .to.be.revertedWith("Caller not authorized");
+      })
+
+    it ("should drain victim out if small wallet's owner sends ether to attacker", async function(){
+      const initialAttackersBalance = await ethers.provider.getBalance(attacker.address);
+      
+      await deployer.sendTransaction({to: this.attackerContract.address, value:1});
+
+      expect(await ethers.provider.getBalance(this.smallWallet.address)).to.eq(0);
+      expect(await ethers.provider.getBalance(attacker.address)).to.eq(
+        initialAttackersBalance.add(10000)
+      );
+
+    })
   });
 });
