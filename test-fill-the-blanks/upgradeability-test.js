ecfafa0f33c2a71eb85c6858cb72a65f82ab3bee
Zeeshan Meghji <zeeshan_meghji@hotmail.com>
2022-01-16 22:43:11 -0500

    completed module 9 (upgradeability)

--- test-fill-the-blanks/upgradeability-test.js
+++ test-fill-the-blanks/upgradeability-test.js
@@ -24,7 +24,34 @@ describe("Upgradable Proxy Pattern", function () {
     it("Should return the address of LogicV1 when calling logicContract()", async function () {
       expect(await this.proxy.logicContract()).to.eq(this.logicV1.address);
     });
-    it("...", async function () {
+    it("Should revert if anyone than the owner tries to upgrade", async function () {
+      await expect(this.proxy.connect(user).upgrade(this.logicV2.address)).to.be.revertedWith("Access restricted");
+    });
+    it("Should allow the owner to update the Logic Contract", async function () {
+      await this.proxy.upgrade(this.logicV2.address);
+      expect(await this.proxy.logicContract()).to.eq(this.logicV2.address);
+    });
+    it("Calling increaseX of LogicV1 should add 1 to x Proxy's state", async function () {
+      await this.proxyPattern.connect(user).increaseX();
+      expect(await this.proxy.x()).to.eq(1);
+      expect(await this.logicV1.x()).to.eq(0);
+
+    });
+    it("Calling increaseX of LogicV2 should add 2 to x Proxy's state", async function () {
+      await this.proxy.upgrade(this.logicV2.address);
+      await this.proxyPattern2.connect(user).increaseX();
+      expect(await this.proxy.x()).to.eq(2);
+      expect(await this.logicV1.x()).to.eq(0);
+    });
+    it("Should set y", async function () {
+      await this.proxy.upgrade(this.logicV2.address);
+      await this.proxyPattern2.connect(user).setY(5);
+      // expect(await this.proxy.owner()).to.eq(5);
+      //get data at memory slot 3
+      const byte32Y = await ethers.provider.getStorageAt(this.proxy.address,3);
+      const y = await ethers.BigNumber.from(byte32Y);
+      expect(y).to.eq(5);
+
     });
   });
 });
